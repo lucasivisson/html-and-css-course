f46b049645be20dc5f94fcb42a0d420bb05ecbe5
Lucas Ivisson <lucasivisson1@gmail.com>
2021-03-26 18:49:14 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+  //abre e fecha menu
+  $('.nav-toggle, .nav-close').click(function(e) {
+    e.preventDefault();
+    $('.nav').toggleClass('active')
+  });
+
+  //fixar header
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 100) {
+      $('.header').addClass('fixed');
+    } else {
+      $('.header').removeClass('fixed');
+    }
+  });
+});
